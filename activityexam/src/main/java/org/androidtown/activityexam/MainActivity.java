f606fe656ade63108bfcd555a11d105451b61d54
devgaram <joingaram@naver.com>
2018-11-24 14:55:04 +0900

    액티비티와 인텐트 예제 (주석 전)

---
+++ activityexam/src/main/java/org/androidtown/activityexam/MainActivity.java
@@ -0,0 +1,55 @@
+package org.androidtown.activityexam;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * MainActivity가 상속받고 있는 AppCompatActivity 클래스의 상속 계층 확인 방법
+ * cursor 올리고 alt+8
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final int REQUEST_CODE=1000;
+    private EditText mNameEditText;
+    private  EditText mAgeEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 화면에 레이아웃 표시
+        setContentView(R.layout.activity_main);
+        // 이름, 나이
+        mNameEditText = (EditText) findViewById(R.id.name_edit);
+        mAgeEditText = (EditText) findViewById(R.id.age_edit);
+        // 버튼 이벤트 처리
+        findViewById(R.id.submit_button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // SecondActivity로 전환하겠다는 intent
+        Intent intent = new Intent(this, SecondActivity.class);
+        // 이름, 나이 가져와서 intent에 추가
+        intent.putExtra("name", mNameEditText.getText().toString());
+        intent.putExtra("age", mAgeEditText.getText().toString());
+        // intent의 정보를 토대로 다른 Activity를 시작
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE
+                && resultCode ==RESULT_OK
+                && data != null) {
+            // 결과를 받음
+            String result = data.getStringExtra("result");
+            //토스트 메시지 표시
+            Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
